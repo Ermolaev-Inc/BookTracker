45e1caed6aa66b0f1efba178b6352b85b1435577
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-09-27 18:19:19 +0500

    Create empty Login & Register

--- book-tracker/pages/index.tsx
+++ book-tracker/pages/index.tsx
@@ -1,12 +1,14 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { IAuthContext } from "../interfaces";
+import Login from "./auth/login";
+import Register from "./auth/register";
 
 export default function App() {
   const authContext: IAuthContext = useContext(AuthContext);
-  
+
   if (authContext.isAuth) {
-    return //App Component
+    return // App Component
   }
-  return //Login Component
+  return <Login />
 }
