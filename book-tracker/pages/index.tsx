da52655c2fe174d98c3c52358efb8b27225419d1
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-09-27 18:13:02 +0500

    Add routing in pages/index

--- book-tracker/pages/index.tsx
+++ book-tracker/pages/index.tsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { IAuthContext } from "../interfaces";
+
 export default function App() {
-  return(
-    <>
-      
-    </>
-  )
+  const authContext: IAuthContext = useContext(AuthContext);
+  
+  if (authContext.isAuth) {
+    return //App Component
+  }
+  return //Login Component
 }
