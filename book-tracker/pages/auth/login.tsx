aeb0c1f665ab1f50b9f96c64c34c95a47cc8aa0d
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-10-03 00:10:29 +0500

    Login Page UI

--- book-tracker/pages/auth/login.tsx
+++ book-tracker/pages/auth/login.tsx
@@ -1,7 +1,21 @@
+import { MainLayout } from "../../layouts/MainLayout";
+import s from "../../styles/Login.module.sass";
+import { Input } from "../../components/Input/Input";
+import { NeonButton } from "../../components/NeonButton/NeonButton";
+
 export default function Login() {
-  return(
-    <>
-      You need to sign in
-    </>
+  return (
+    <MainLayout title="Sign in">
+      <div className={s.wrapper}>
+        <img src="/Login-LeftSide.svg" alt="" className={s.leftSide}/>
+        <div className={s.rightSide}>
+          <img src="/BookTrackerLogo.svg" alt="" className={s.logo}/>
+          <Input placeholder="Login" type="text" />
+          <Input placeholder="Password" type="password"/>
+          <NeonButton title="Sign in" color="#75c0ff"/>
+          <NeonButton title="Sign up" color="#ff9627"/>
+        </div>
+      </div>
+    </MainLayout>
   )
 }
