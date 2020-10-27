7bdacd644b016138671e5bf42432d832c1a6d4b3
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-10-28 02:24:14 +0500

    Setup login component and styels

--- ClientApp/src/pages/Login.tsx
+++ ClientApp/src/pages/Login.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import s from "../styles/Login.module.sass";
+import leftSide from "../img/leftSide.svg";
+import logo from "../img/logo.svg";
 
 export const Login = () => {
+  // TODO: Neon Buttons & Inputs
   return(
-    <>
-      // TODO
-    </>
+    <div className={s.wrapper}>
+      <img className={s.leftSide} src={leftSide} alt=""/>
+      <div className={s.rightSide}>
+        <img src={logo} alt=""/>
+        <input type="text"/>
+        <input type="password"/>
+        <button>Sign in</button>
+        <button>Sign up</button>
+      </div>
+    </div>
   );
 };
