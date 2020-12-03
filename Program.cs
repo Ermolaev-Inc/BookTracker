669d181f2f52e8ef2e7f9d4450fc4a42c78f81f8
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-12-03 12:14:59 +0500

    Connect PostgreSQL

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 
 namespace BookTracker
 {
@@ -13,11 +15,30 @@ namespace BookTracker
     {
         public static void Main(string[] args)
         {
+            TestConnection();
+            Console.ReadKey();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+        private static void TestConnection()
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                con.Open();
+                if (con.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Connected");
+                }
+            }
+        }
+        
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=ilyaermolaev;Database=booktracker;");
+        }
     }
 }
